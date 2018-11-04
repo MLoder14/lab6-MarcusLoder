a3108f009a91872891ea3dafb1aed123b173f5bf
Marcus <loderm14@my.lanecc.edu>
2018-11-04 01:44:45 -0600

    Hello Clarice

---
+++ Lab6-MarcusLoder/MainActivity.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using System.Collections.Generic;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+
+namespace Lab6_MarcusLoder
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        Tide[] periods;
+
+
+        // Overriding OnCreate in the Activity super-class
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            
+
+            // Development testing: load data into the ArrayAdapter by filling the array with Flora objects with "hard-coded" names
+            /*
+			items = new Flora[] {
+				new Flora { Name = "Vegetables"},
+				new Flora { Name = "Fruits"},
+				new Flora { Name = "Flower Buds"},
+				new Flora { Name = "Legumes"},
+				new Flora { Name = "Bulbs"},
+				new Flora { Name = "Tubers"}
+			};
+			*/
+
+            // Load data into the ArrayAdapter by filling the array with Flora objects with names read from a file
+            periods = LoadListOfTidesFromAssets();
+
+            // Assign our ArrayAdapter to the ListActivity's ListAdapter property
+            ListAdapter = new HomeScreenAdapter(this, items);
+
+            // This is all you need to do to enable fast scrolling
+            ListView.FastScrollEnabled = true;
+
+        }
+
+        // Overriding an event handler in the ListActivity super-class
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            var tides = periods[position];
+            Android.Widget.Toast.MakeText(this, tides.ToString(), Android.Widget.ToastLength.Short).Show();)
+        }
+
+        private Tide[] LoadListofTidesFromAssets()
+        {
+            var tides = new List<Tide>();
+            var seedDataSteam = Assets.Open(@"TideData.txt");
+            using (var reader = new StreamReader(seedDataSteam))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var tide = new Tide { TypeandName = reader.ReadLine() };
+                    tides.Add(tide);
+                }
+            }
+            tides.Sort((x,y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+
+            periods = tides.ToArray();
+
+            return periods;
+        }
+    }
+}
+
+/************************************************************************
+   ************************  HomeScreenAdapter class  **********************
+   *************************************************************************/
+public class Adapter : ArrayAdapter<string>
+{
+    string[] periods;
+    Activity context;
+
+    public Adapter(Activity context, string[] periods) : base()
+    {
+        this.context = context;
+        this.periods = periods;
+    }
+
+    public override long GetItemId(int position)
+    {
+        return position; // base.GetItemId(position);
+    }
+
+    public override string this[int position]
+    {
+        get { return periods[position]; }
+    }
+
+    public override int Count
+    {
+        get { return periods.Length; }
+    }
+
+    public override View GetView(int position, View convertView, ViewGroup parent)
+    {
+        View view = convertView; // re-use an existing view, if one is available
+        if (view == null) // otherwise create a new one
+            view = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+        view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = periods[position];
+        return view;
+        //return base.GetView(position, convertView, parent);
+    }
+
+    /* -- Code for the ISectionIndexer implementation follows -- */
+
+    String[] sections;
+    Java.Lang.Object[] sectionsObjects;
+    Dictionary<string, int> alphaIndex;
+
+    public int GetPositionForSection(int section)
+    {
+        return alphaIndex[sections[section]];
+    }
+
+    public int GetSectionForPosition(int position)
+    {
+        return 1;
+    }
+
+    public Java.Lang.Object[] GetSections()
+    {
+        return sectionsObjects;
+    }
+
+    private void BuildSectionIndex()
+    {
+        alphaIndex = new Dictionary<string, int>();
+        for (var i = 0; i < periods.Length; i++)
+        {
+            // Use the first character in the name as a key.
+            var key = periods[i].Name.Substring(0, 1);
+            if (!alphaIndex.ContainsKey(key))
+            {
+                alphaIndex.Add(key, i);
+            }
+        }
+
+        sections = new string[alphaIndex.Keys.Count];
+        alphaIndex.Keys.CopyTo(sections, 0);
+        sectionsObjects = new Java.Lang.Object[sections.Length];
+        for (var i = 0; i < sections.Length; i++)
+        {
+            sectionsObjects[i] = new Java.Lang.String(sections[i]);
+        }
+    }
+
+}
+
+
+/************************************************************************
+************************  HomeScreenAdapter class  **********************
+*************************************************************************/
+
+/// <summary>
+///   A simple class for holding the data that is read from VegeData.txt
+/// </summary>
+public class Flora
+{
+    public string Name { get; set; }
+    public override string ToString()
+    {
+        return Name;
+    }
+} 
+}
---
+++ Lab6-MarcusLoder/XmlFileReader.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Xml;
+using System.IO;
+
+namespace Lab6_MarcusLoder
+{
+    public class XmlVocabFileParserTides
+    {
+        // constants to use for XML element names and dictionary keys
+        /*public const string SPANISH = "spanish";
+		public const string ENGLISH = "english";
+		public const string POS = "pos"; // part of speech
+		public const string WORD = "word";   // XML element containing spanish, english, and pos elements*/
+
+        public const string HIGH = "highlow";
+        public const string HEIGHT = "pred_in_cm";
+        public const string TIME = "time";
+        public const string DAY = "day";
+        public const string DATE = "date"; // part of speech
+        public const string Item = "item";   // XML element containing spanish, english, and pos elements
+                                             // This list will be filled with dictionary objects
+        List<IDictionary<string, object>> tideList;   // backing variable for TideList property
+
+        public List<IDictionary<string, object>> TideList { get { return tideList; } }
+
+        public XmlVocabFileParserTides(Stream xmlStream)
+        {
+            // SimpleAdapter requires a list of JavaDictionary<string,object> objects
+            tideList = new List<IDictionary<string, object>>();
+
+            /*
+			//for testing
+			var item1 = new JavaDictionary<string, object> ();
+			item1.Add (SPANISH, "mono");
+			item1.Add (ENGLISH, "monkey");
+			item1.Add ("partOfSpeech", "noun");
+			vocabList.Add(item1);
+			var item2 = new JavaDictionary<string, object> ();
+			item2.Add (SPANISH, "agua");
+			item2.Add (ENGLISH, "water");
+			item2.Add (ENGLISH, "noun");
+			vocabList.Add(item2);
+			var item3 = new JavaDictionary<string, object> ();
+			item3.Add (SPANISH, "saltar");
+			item3.Add (ENGLISH, "to jump");
+			item3.Add (ENGLISH, "verb");
+			vocabList.Add(item3);
+			*/
+
+            // Parse the xml file and fill the list of JavaDictionary objects with vocabulary data
+            using (XmlReader reader = XmlReader.Create(xmlStream))
+            {
+                JavaDictionary<string, object> tide = null;
+                while (reader.Read())
+                {
+                    // Only detect start elements.
+                    if (reader.IsStartElement())
+                    {
+                        // Get element name and switch on it.
+                        switch (reader.Name)
+                        {
+                            case Item:
+                                // New word
+                                tide = new JavaDictionary<string, object>();
+                                break;
+                            case TIME:
+                                // Add spanish word
+                                if (reader.Read() && tide != null)
+                                {
+                                    tide.Add(TIME, reader.Value.Trim());
+                                }
+                                break;
+                            case DAY:
+                                // Add Day
+                                if (reader.Read() && tide != null)
+                                {
+                                    tide.Add(DAY, reader.Value.Trim());
+                                }
+                                break;
+                            case DATE:
+                                // Add Date
+                                if (reader.Read() && tide != null)
+                                {
+                                    tide.Add(DATE, reader.Value.Trim());
+                                }
+                                break;
+                            case HEIGHT:
+                                // Add part height
+                                if (reader.Read() && tide != null)
+                                {
+                                    tide.Add(HEIGHT, reader.Value.Trim());
+                                }
+                                break;
+                            case HIGH:
+                                // Add High or Low
+                                if (reader.Read() && tide != null)
+                                {
+                                    if (reader.Value.Trim() == "H")
+                                    {
+                                        tide.Add(HIGH, "High");
+                                    }
+                                    else
+                                    {
+                                        tide.Add(HIGH, "Low");
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                    else if (reader.Name == Item)
+                    {
+                        // reached </word>
+                        tideList.Add(tide);
+                        tide = null;
+                    }
+
+                }
+            }
+
+        }
+    }
+}
